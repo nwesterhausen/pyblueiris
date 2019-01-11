9cf107bd857f09b0c04279c4282813f206f5c6be
Nicholas Westerhausen <nwesterhausen@gmail.com>
2019-01-11 10:11:05 -0500

    testing enable/disable camera

--- test.py
+++ test.py
@@ -22,10 +22,10 @@ async def tests():
 
         print(blue.attributes)
 
-        print("Sending disable to INT01")
-        await blue.enable_camera("INT01", False)
-        print("Sending enable to INT01")
-        await blue.enable_camera("INT01", True)
+        # print("Sending disable to INT01")
+        # await blue.enable_camera("INT01", False)
+        # print("Sending enable to INT01")
+        # await blue.enable_camera("INT01", True)
 
 
 if __name__ == "__main__":
